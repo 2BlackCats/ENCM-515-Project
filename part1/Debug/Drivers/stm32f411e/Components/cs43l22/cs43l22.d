e1ca41070e345568c26e93db6868deeb05771fe7
julienrabino <julienmescha.rabino@ucalgary.ca>
2025-03-20 19:48:05 -0600

    added all code into main.c

---
+++ part1/Debug/Drivers/stm32f411e/Components/cs43l22/cs43l22.d
@@ -0,0 +1,6 @@
+Drivers/stm32f411e/Components/cs43l22/cs43l22.o: \
+ ../Drivers/stm32f411e/Components/cs43l22/cs43l22.c \
+ ../Drivers/stm32f411e/Components/cs43l22/cs43l22.h \
+ C:/Users/julie/school/3rd/encm515/lab_materials_2025/final_proj/ENCM-515-Project/part1/Drivers/stm32f411e/Inc/../Common/audio.h
+../Drivers/stm32f411e/Components/cs43l22/cs43l22.h:
+C:/Users/julie/school/3rd/encm515/lab_materials_2025/final_proj/ENCM-515-Project/part1/Drivers/stm32f411e/Inc/../Common/audio.h:
---
+++ part1/Debug/Drivers/stm32f411e/Components/i3g4250d/i3g4250d.d
@@ -0,0 +1,6 @@
+Drivers/stm32f411e/Components/i3g4250d/i3g4250d.o: \
+ ../Drivers/stm32f411e/Components/i3g4250d/i3g4250d.c \
+ ../Drivers/stm32f411e/Components/i3g4250d/i3g4250d.h \
+ C:/Users/julie/school/3rd/encm515/lab_materials_2025/final_proj/ENCM-515-Project/part1/Drivers/stm32f411e/Inc/../Common/gyro.h
+../Drivers/stm32f411e/Components/i3g4250d/i3g4250d.h:
+C:/Users/julie/school/3rd/encm515/lab_materials_2025/final_proj/ENCM-515-Project/part1/Drivers/stm32f411e/Inc/../Common/gyro.h:
